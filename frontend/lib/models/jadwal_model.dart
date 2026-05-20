5bfb209f279d71c23b91b293981a99fc064f0ea8
Fazril <fazwyanta@gmail.com>
2026-05-20 11:01:33 +0700

    Fazril Wiryamanta / 2402310211 : Penambahan model jadwal dan mata kuliah untuk penjadwalan dan manajemen mata kuliah

---
+++ frontend/lib/models/jadwal_model.dart
@@ -0,0 +1,15 @@
+class JadwalModel {
+  final String namaMatkul;
+  final String hari;
+  final String jamMulai;
+  final String jamSelesai;
+  final String ruangan;
+
+  JadwalModel({
+    required this.namaMatkul,
+    required this.hari,
+    required this.jamMulai,
+    required this.jamSelesai,
+    required this.ruangan,
+  });
+}
---
+++ frontend/lib/models/matkul_model.dart
@@ -0,0 +1,11 @@
+class MatkulModel {
+  final String nama;
+  final String dosen;
+  final String semester;
+
+  MatkulModel({
+    required this.nama,
+    required this.dosen,
+    required this.semester,
+  });
+}
