7ad80ed9e4f55f6e3d476256383e02f0102d0f14
Fritzzzzzzzz <frizqi644@gmail.com>
2026-05-21 01:49:14 +0700

    Muhammad Rizqi Firdaus / 240231019 : Menambahkan screen Tugas dan Profile pada MainScreen serta memperbaiki struktur kode

--- frontend/lib/screens/main/main_screen.dart
+++ frontend/lib/screens/main/main_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../dashboard/dashboard_screen.dart';
 import '../jadwal/jadwal_screen.dart';
+import '../tugas/tugas_screen.dart';
+import '../profile/profile_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,11 +13,9 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-
   int selectedIndex = 0;
 
   void changeTab(int index) {
-
     setState(() {
       selectedIndex = index;
     });
@@ -23,36 +23,30 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     final List<Widget> pages = [
-
-      DashboardScreen(
-        onTabChange: changeTab,
-      ),
+      DashboardScreen(onTabChange: changeTab),
 
       const JadwalScreen(),
+      const TugasScreen(),
+      const ProfileScreen(),
     ];
 
     return Scaffold(
-
       body: pages[selectedIndex],
 
       bottomNavigationBar: BottomNavigationBar(
-
         currentIndex: selectedIndex,
 
         selectedItemColor: const Color(0xFF1A58B7),
         unselectedItemColor: Colors.grey,
 
         onTap: (index) {
-
           setState(() {
             selectedIndex = index;
           });
         },
 
         items: const [
-
           BottomNavigationBarItem(
             icon: Icon(Icons.dashboard),
             label: 'Dashboard',
@@ -62,6 +56,9 @@ class _MainScreenState extends State<MainScreen> {
             icon: Icon(Icons.calendar_today),
             label: 'Jadwal',
           ),
+
+          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'Tugas'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
       ),
     );
