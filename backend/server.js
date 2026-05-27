feb3736e436f3be02bf8b245e9251f671dfee421
Fritzzzzzzzz <frizqi644@gmail.com>
2026-05-28 03:45:34 +0700

    Muhammad Rizqi Firdaus / 2402310196 : Inisialisasi backend Express.js dengan konfigurasi .env di dalam server.js

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv")
+    .config();
+
+const app =
+    require("./src/app");
+
+const PORT =
+    process.env.PORT ||
+    3000;
+
+app.listen(
+    3000,
+    "0.0.0.0",
+    () => {
+
+        console.log(
+            "Server jalan di port 3000"
+        );
+
+    });
+
