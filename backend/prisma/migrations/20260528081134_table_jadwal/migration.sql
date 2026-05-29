785c60d4113fbe0caec5a0e915f5de59be6eead0
Raditzx-ez <raditman555@gmail.com>
2026-05-29 07:10:11 +0700

    Raditia Khairul Muttaqin / 2402310206 : Menambahkan model jadwal dan model matkul pada skema prisma dan migrasi ke database

---
+++ backend/prisma/migrations/20260528081134_table_jadwal/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `Jadwal` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `namaMatkul` VARCHAR(191) NOT NULL,
+    `hari` VARCHAR(191) NOT NULL,
+    `jamMulai` VARCHAR(191) NOT NULL,
+    `jamSelesai` VARCHAR(191) NOT NULL,
+    `ruangan` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Jadwal` ADD CONSTRAINT `Jadwal_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ backend/prisma/migrations/20260528101402_table_matkul/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Matkul` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nama` VARCHAR(191) NOT NULL,
+    `dosen` VARCHAR(191) NOT NULL,
+    `semester` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Matkul` ADD CONSTRAINT `Matkul_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
