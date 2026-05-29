8e557303f2a7932f81cfecc85ae9e29c16c7ddb2
Raditzx-ez <raditman555@gmail.com>
2026-05-30 02:43:26 +0700

    Raditia Khairul Muttaqin / 2402310206 : Menambahkan properti id dan metode untuk konversi JSON pada model Jadwal

--- frontend/lib/models/jadwal_model.dart
+++ frontend/lib/models/jadwal_model.dart
@@ -1,15 +1,59 @@
 class JadwalModel {
+  final int? id;
+
   final String namaMatkul;
+
   final String hari;
+
   final String jamMulai;
+
   final String jamSelesai;
+
   final String ruangan;
 
   JadwalModel({
+    this.id,
+
     required this.namaMatkul,
+
     required this.hari,
+
     required this.jamMulai,
+
     required this.jamSelesai,
+
     required this.ruangan,
   });
+
+  factory JadwalModel.fromJson(Map<String, dynamic> json) {
+    return JadwalModel(
+      id: json["id"],
+
+      namaMatkul: json["namaMatkul"],
+
+      hari: json["hari"],
+
+      jamMulai: json["jamMulai"],
+
+      jamSelesai: json["jamSelesai"],
+
+      ruangan: json["ruangan"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+
+      "namaMatkul": namaMatkul,
+
+      "hari": hari,
+
+      "jamMulai": jamMulai,
+
+      "jamSelesai": jamSelesai,
+
+      "ruangan": ruangan,
+    };
+  }
 }
