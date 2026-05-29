b8ace300e42288ef2524593bc9fe0e9105ab0136
mohsyarifjauhari <moh.syarifjauhari2024.student@unibamadura.ac.id>
2026-05-30 03:19:12 +0700

    Moh. Syarif Jauhari / 2402310153 : Menambahkan properti id dan metode untuk konversi JSON pada model Matkul

--- frontend/lib/models/matkul_model.dart
+++ frontend/lib/models/matkul_model.dart
@@ -1,11 +1,35 @@
 class MatkulModel {
+  final int? id;
+
   final String nama;
+
   final String dosen;
+
   final String semester;
 
   MatkulModel({
+    this.id,
+
     required this.nama,
+
     required this.dosen,
+
     required this.semester,
   });
+
+  factory MatkulModel.fromJson(Map<String, dynamic> json) {
+    return MatkulModel(
+      id: json["id"],
+
+      nama: json["nama"],
+
+      dosen: json["dosen"],
+
+      semester: json["semester"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"id": id, "nama": nama, "dosen": dosen, "semester": semester};
+  }
 }
