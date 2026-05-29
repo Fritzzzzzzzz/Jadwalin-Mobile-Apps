cdafe94ae0cf8217046f9d9ff615e6997fc39ff0
mohsyarifjauhari <moh.syarifjauhari2024.student@unibamadura.ac.id>
2026-05-30 03:27:01 +0700

    Moh. Syarif Jauhari / 2402310153 : Implementasi MatkulService dengan operasi CRUD untuk manajemen mata kuliah

---
+++ frontend/lib/services/matkul_service.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/matkul_model.dart';
+
+class MatkulService {
+  static const String baseUrl = "http://192.168.1.3:3000/api/matkul";
+
+  // GET
+  Future<List<MatkulModel>> getMatkul() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final token = prefs.getString("token");
+
+    final response = await http.get(
+      Uri.parse(baseUrl),
+
+      headers: {"Authorization": "Bearer $token"},
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      final List matkulList = data["data"];
+
+      return matkulList.map((e) => MatkulModel.fromJson(e)).toList();
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+
+  // TAMBAH
+  Future<void> tambahMatkul(MatkulModel matkul) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final token = prefs.getString("token");
+
+    final response = await http.post(
+      Uri.parse(baseUrl),
+
+      headers: {
+        "Content-Type": "application/json",
+
+        "Authorization": "Bearer $token",
+      },
+
+      body: jsonEncode({
+        "nama": matkul.nama,
+
+        "dosen": matkul.dosen,
+
+        "semester": matkul.semester,
+      }),
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode != 201) {
+      throw Exception(data["message"]);
+    }
+  }
+
+  // EDIT
+  Future<void> editMatkul(MatkulModel matkul) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final token = prefs.getString("token");
+
+    final response = await http.patch(
+      Uri.parse("$baseUrl/${matkul.id}"),
+
+      headers: {
+        "Content-Type": "application/json",
+
+        "Authorization": "Bearer $token",
+      },
+
+      body: jsonEncode({
+        "nama": matkul.nama,
+
+        "dosen": matkul.dosen,
+
+        "semester": matkul.semester,
+      }),
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode != 200) {
+      throw Exception(data["message"]);
+    }
+  }
+
+  // HAPUS
+  Future<void> hapusMatkul(int id) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final token = prefs.getString("token");
+
+    final response = await http.delete(
+      Uri.parse("$baseUrl/$id"),
+
+      headers: {"Authorization": "Bearer $token"},
+    );
+
+    final data = jsonDecode(response.body);
+
+    if (response.statusCode != 200) {
+      throw Exception(data["message"]);
+    }
+  }
+}
