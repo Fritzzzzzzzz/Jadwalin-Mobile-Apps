0a91374046f2bee9b35b6e0f4aff533ae928a580
RonyRafliansyah <ronyrafliansyah168@gmail.com>
2026-06-01 21:34:23 +0700

    M. Rony Rafliansyah / 2402310168 : Penyesuaian model data tugas dengan struktur database

--- frontend/lib/models/tugas_model.dart
+++ frontend/lib/models/tugas_model.dart
@@ -1,16 +1,45 @@
 class TugasModel {
-  final String matkul;
-  final String judul;
-  final String deadline;
-  final String? deskripsi;
+  final int? id;
 
-  String status;
+  final int matkulId;
+  final String namaMatkul;
+
+  final String judul;
+  final String? deskripsi;
+  final String deadline;
+  final String status;
 
   TugasModel({
-    required this.matkul,
+    this.id,
+    required this.matkulId,
+    required this.namaMatkul,
     required this.judul,
-    required this.deadline,
     this.deskripsi,
-    this.status = 'Belum Selesai',
+    required this.deadline,
+    required this.status,
   });
+
+  factory TugasModel.fromJson(Map<String, dynamic> json) {
+    return TugasModel(
+      id: json["id"],
+      matkulId: json["matkulId"],
+      namaMatkul: json["namaMatkul"],
+      judul: json["judul"],
+      deskripsi: json["deskripsi"],
+      deadline: json["deadline"],
+      status: json["status"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "matkulId": matkulId,
+      "namaMatkul": namaMatkul,
+      "judul": judul,
+      "deskripsi": deskripsi,
+      "deadline": deadline,
+      "status": status,
+    };
+  }
 }
